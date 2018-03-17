368ba60ebcd573909c44dc43db49751d6108059e
Florent Gravin <florent.gravin@camptocamp.com>
2018-03-17 14:12:38 +0100

    Add running scripts

---
+++ formatter.sh
@@ -0,0 +1,2 @@
+cp -R -f schemas/iso19139/src/main/plugin/iso19139/ web/src/main/webapp/WEB-INF/data/config/schema_plugins/
+
---
+++ install.sh
@@ -0,0 +1,1 @@
+mvn clean install -DskipTests -Ddb.type=postgres -Ddb.username=geonetwork -Ddb.password=geonetwork -Ddb.name=ne_gn3
